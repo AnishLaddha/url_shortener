ef32122c429a3c96f1b4059afd9ee564416d62bb
Anish Laddha <anishladdha03@gmail.com>
2021-03-23 22:53:29 -0700

    added url mongoose schema

---
+++ models/url.js
@@ -0,0 +1,23 @@
+import mongoose, { mongo, Mongoose } from 'mongoose';
+const urlSchema = new mongoose.Schema(
+    {
+        longUrl: {
+            type: String,
+            required: true,
+        },
+        shortUrl: {
+            type: String,
+            unique: true,
+            required: true,
+        },
+        clicks: {
+            type: Number,
+            default: 0,            
+        }
+
+    },
+    {timestamps: true}
+
+);
+const URL = mongoose.model('URL', urlSchema);
+export default URL;
